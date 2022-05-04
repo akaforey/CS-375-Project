b98ae7f7b71a6680f231219a5a35d56d3e459370
Alex Kaforey <akafore1@binghamton.edu>
2022-05-04 14:31:01 -0400

    compiles and runs, queue has bugs

--- main.cpp
+++ main.cpp
@@ -57,7 +57,7 @@ cout << "heap tree completed" << endl;
 
 
     //Queue code
-    DataNode data_nodes[text.size()];
+    // DataNode data_nodes[text.size()];
     char sorted[text.size()];
     int k = 0;
     for (const auto &j : text) {
@@ -72,34 +72,79 @@ cout << "heap tree completed" << endl;
         sorted[k] = min_char;
         k++;
     }
-    Queue queue1(text.size(),data_nodes), queue2(text.size(),data_nodes);
+    // Queue queue1(text.size(),data_nodes), queue2(text.size(),data_nodes);
+    Queue queue1; // leaves
+    Queue queue2; // trees
     for (int i = 0; i < text.size(); i++) {
-        data_nodes[i].data = sorted[i];
-        data_nodes[i].freq = text[sorted[i]];
-        queue1.enqueue(&data_nodes[i]);
+        // data_nodes[i].data = sorted[i];
+        // data_nodes[i].freq = text[sorted[i]];
+        // queue1.enqueue(&data_nodes[i]);
+        queue1.enqueue(new node(sorted[i], text[sorted[i]]));
     }
 
-    while (queue1.head != 0 || queue2.head != 0) {
-         DataNode *node1, *node2;
-        if (queue2.head == 0) {
-            node1 = queue1.dequeue();
-            if (queue1.head != 0)
-                node2 = queue1.dequeue();
-        }
-        else if (queue1.head == 0) {
-            node1 = queue2.dequeue();
-            if (queue2.head != 0)
-            node2 = queue2.dequeue();
-        }
-        else {
-            if (queue1.tail->freq < queue2.tail->freq) {
-                node1 = queue1.dequeue();
+
+    for (int loops = 0; loops < text.size()-1; loops++){
+        node* x;
+        node* y;
+        // find x
+        if (queue1.head && queue2.head){
+            if (queue1.head->n->freq <= queue2.head->n->freq) {
+                x = queue1.dequeue();
             }
             else {
-                node1 = queue2.dequeue();
+                x = queue2.dequeue();
             }
+        } else if (queue1.head) {
+            x = queue1.dequeue();
+        } else if (queue2.head) {
+            x = queue2.dequeue();
         }
+        // find y
+        if (queue1.head && queue2.head){
+            if (queue1.head->n->freq <= queue2.head->n->freq) {
+                y = queue1.dequeue();
+            }
+            else {
+                y = queue2.dequeue();
+            }
+        } else if (queue1.head) {
+            y = queue1.dequeue();
+        } else if (queue2.head) {
+            y = queue2.dequeue();
+        }
+        // make z
+        node* z = new node((x->freq)+(y->freq), 0); // Internal node
+        z->left = x;
+        z->right = y;
+        queue2.enqueue(z);
     }
+
+    node* huffman_tree_2 = queue2.dequeue();
+cout << "Printing tree 2:" << endl;
+    huffman_tree_2->printBT("", 0);
+cout << "queue tree completed" << endl;
+
+    // while (queue1.head != 0 || queue2.head != 0) {
+    //      DataNode *node1, *node2;
+    //     if (queue2.head == 0) {
+    //         node1 = queue1.dequeue();
+    //         if (queue1.head != 0)
+    //             node2 = queue1.dequeue();
+    //     }
+    //     else if (queue1.head == 0) {
+    //         node1 = queue2.dequeue();
+    //         if (queue2.head != 0)
+    //         node2 = queue2.dequeue();
+    //     }
+    //     else {
+    //         if (queue1.tail->freq < queue2.tail->freq) {
+    //             node1 = queue1.dequeue();
+    //         }
+    //         else {
+    //             node1 = queue2.dequeue();
+    //         }
+    //     }
+    // }
 
     in_file.close();
     return 0;
--- queue.h
+++ queue.h
@@ -1,44 +1,97 @@
-struct DataNode {
-    DataNode *next; 
-    DataNode *prev;
-    char data;
-    unsigned long freq;
+#ifndef QUEUE_H
+#define QUEUE_H
+
+#include "node.h"
+
+class DataNode {
+    public:
+    DataNode *next = nullptr; 
+    DataNode *prev = nullptr;
+    node* n;
+    DataNode(node* n){
+        this->n = n;
+    }
+    // char data;
+    // unsigned long freq;
 };
+
 
 class Queue {
     public:
         Queue(unsigned int, DataNode*);
-        void enqueue(DataNode*);
-        DataNode* dequeue();
-        DataNode *head;
-        DataNode *tail;
+        Queue();
+        // void enqueue(DataNode*);
+        void enqueue(node*);
+        node* dequeue();
+        // DataNode* dequeue();
+        DataNode *head = nullptr;
+        DataNode *tail = nullptr;
 };
+
+Queue::Queue(){
+    this->head = nullptr;
+}
 
 Queue::Queue(unsigned int char_count, DataNode *array) {
     this->head = 0;
     this->tail = 0;
 }
 
-void Queue::enqueue(DataNode* n) {
-    if (this->head == 0) {
-        this->head = n;
-        n->prev = 0;
-    } else {
-        n->prev = this->tail;
-        n->prev->next = n;
+void Queue::enqueue(node* n){
+    DataNode* temp = new DataNode(n);
+    if (this->head == nullptr){
+        this->head = temp;
+        this->tail = temp;
+        return;
     }
-    this->tail = n;
-    n->next = 0;
+    else {
+        this->tail->next = temp;
+        temp->prev = this->tail;
+        temp->next = nullptr;
+        this->tail = temp;
+    }
+    return;
+
 }
 
-DataNode* Queue::dequeue() {
-    if (this->tail == 0) {
-        return 0;
+// void Queue::enqueue(DataNode* n) {
+//     if (this->head == 0) {
+//         this->head = n;
+//         n->prev = 0;
+//     } else {
+//         n->prev = this->tail;
+//         n->prev->next = n;
+//     }
+//     this->tail = n;
+//     n->next = 0;
+// }
+
+node* Queue::dequeue() {
+    node* ret_val;
+    if (this->tail == nullptr){
+        return nullptr;
     } else {
-        DataNode *ret_node = this->tail;
+        ret_val = this->tail->n;
         this->tail = this->tail->prev;
-        if (this->tail != 0) {
-            this->tail->next = 0;
+        if (this->tail){
+            this->tail->next = nullptr;
+        } else {
+            this->head = nullptr;
         }
     }
+    return ret_val;
 }
+
+// DataNode* Queue::dequeue() {
+//     if (this->tail == 0) {
+//         return 0;
+//     } else {
+//         DataNode *ret_node = this->tail;
+//         this->tail = this->tail->prev;
+//         if (this->tail != 0) {
+//             this->tail->next = 0;
+//         }
+//     }
+// }
+
+#endif
